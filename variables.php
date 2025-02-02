98f3d75e5582b7ac20da0b1bd585ee0072bbec1a
Jonev-29 <journeynevaeh29@gmail.com>
2025-02-02 06:23:05 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset=utf-8">
+        <title>Variables</title>
+</head>
+<body>
+    <?php // Script 2.3 - variables.php
+
+    // first and last name:
+    $first_name = "Journey";
+    $last_name = "Nevaeh";
+
+    // An address:
+    $street = "1205 Sandretto Dr";
+    $city = "Prescott";
+    $state = "AZ";
+    $zip = "86305";
+
+    // a phone number:
+    $phone = "520-555-2468";
+
+    // print the info:
+    print "<p>My name is $first_name $last_name</p>";
+    print "<p>The address is:<br>$street<br>$city $state $zip</p>";
+    print "<p>The totally real phone number is $phone</p>";
+
+    ?>
+    </body>
+    </html>
