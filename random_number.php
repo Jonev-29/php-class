fb51743ec64df2fb05c07cf44acede36b5c7d0db
Jonev-29 <journeynevaeh29@gmail.com>
2025-02-09 08:14:31 -0700

    Add files via upload

---
+++ random_number.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Lucky Numbers</title>
+</head>
+<body>
+<?php
+echo(rand(10,100) . "<br>");
+?>
+</body>
+</html>
