a57d161b2adb3595976d572f6a7e86f0e5773c4c
Jonev-29 <journeynevaeh29@gmail.com>
2025-02-17 02:14:49 -0700

    Add files via upload

---
+++ handle_reg.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Registration</title>
+</head>
+<body>
+    <h1>Registration Results</h1>
+    <?php // recieves values from register.html
+
+    // Address error management
+    ini_set('display errors',0);
+    error_reporting (E_ALL);
+
+    $okay = true;
+
+    if ($okay) {
+        print '<p>You have been successfully registered (but not really)';
+    }
+    ?>
+    </body>
+    </html>
