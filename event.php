dc660dbe3101623e8fe0a07365030d15d7fda6d2
Jonev-29 <journeynevaeh29@gmail.com>
2025-02-23 02:20:40 -0700

    Add files via upload

---
+++ event.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Add an Event</title>
+    </head>
+    <body>
+        <?php
+            print "<p>You want to add an event called <b>{$_POST['name']}</b> which takes place on: <br>";
+
+            // print each weekday
+            if (isset($_POST['days']) AND is_array($_POST['days'])){
+                foreach ($_POST['days'] as $day) {
+                    print "$day<br>\n";
+                }
+            } else {
+                print 'Please select at least one weekday for this event!';
+            }
+
+            // complete paragraph
+            print '</p>';
+
+
+        ?>
+    </body>
+</html>
