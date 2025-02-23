95720134253186061e082b05212a229185bf2cb2
Jonev-29 <journeynevaeh29@gmail.com>
2025-02-23 03:13:32 -0700

    Add files via upload

--- books.php
+++ books.php
@@ -1,48 +1,19 @@
-<!doctype html>
-<html lang="en">
-    <meta charset="utf-8">
-    <title>Larry Ulman's Books and Chapters</title>
-</head>
-<body>
-    <h1>Some of Larry Ulamn's Books</h1>
-    <?php
+<?php
 
-    // create first array
-    $phpvqs = [
-        1 => 'Getting Started with PHP', 'Variables', 'HTML Forms and PHP', 'Using Numbers'
-    ];
+// set page title and include header file
+define('TITLE', 'Books by J.D. Salinger');
+include('templates/header.html');
 
-    // create second array
-    $phpadv = [
-        1 => 'Advanced PHP Techniques', 'Developing Web Applications', 'Advanced Database Concepts', 'Basic Object-Oriented Programming'
-    ];
+?>
 
-    // create third array
-    $phpmysql = [
-        1 => 'Introduction to PHP', 'Programming with PHP', 'Creating Dynamic Web Sites', 'Introduction to MySQL'
-    ];
+<h2>J.D. Salinger's Books</h2>
+<ul>
+    <li>The Catcher in the Rye</li>
+    <li>Nine Stories</li>
+    <li>Franny and Zooey</li>
+    <li>Raise High the Roof Beam, Carpenters and Seymour: An Introduction</li>
+</ul>
 
-    // create multi dimensional array
-    $books = [
-        'PHP VQS' => $phpvqs,
-        'PHP Advanced VQP' => $phpadv,
-        'PHP and MySQL VQP' => $phpmysql
-    ];
-
-    // print out some values
-    print "<p>The Third chapter of my first book is <i>{$books['PHP VQS'][3]}</i>.</p>";
-    print "<p>The first chapter of my second book is <i>{$books['PHP Advanced VQP'][1]}</i>.</p>";
-    print "<p>The fourth chapter of my third book is <i>{$books['PHP and MySQL VQP'][4]}</i>.</p>";
-
-    // print with foreach
-    foreach ($books as $title => $chapters) {
-        print "<p>$title";
-        foreach ($chapters as $number => $chapter){
-            print "<br/>Chapter $number is $chapter";
-        }
-        print '</p>';
-    }
-
-    ?>
-    </body>
-    </html>
+<?php
+include('templates/footer.html');
+?>
